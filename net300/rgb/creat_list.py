520472b05b6734da479b8c4d775045220f579393
Fereshteh Aghaee <49758163+angel9785@users.noreply.github.com>
2023-12-03 16:17:42 +0330

    Delete net300 directory

--- net300/rgb/creat_list.py
+++
@@ -1,9 +0,0 @@
-data_folder = "/home/fereshteh/codergb_new"
-kaist_path="/home/fereshteh/kaist/day"
-check="/home/fereshteh/code/SSD_MobileNet-master/voc_checkpoint_ssd300.pth.tar"
-import torch
-c=torch.load(check)
-print(c["loss"])
-
-
-
